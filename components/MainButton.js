328867b47b4892d70d2a78ae3314b38c3dbcf1c4
FabriRejala <fa.rejala03@gmail.com>
2023-05-04 18:01:56 -0400

    First layout, input and buttons

---
+++ components/MainButton.js
@@ -0,0 +1,39 @@
+import { View, Text, Pressable, StyleSheet } from "react-native";
+
+const MainButton = ({ children }) => {
+  pressHandler = () => {
+    console.log("pressed");
+  };
+
+  return (
+    <View style={styles.buttonOuterContainer}>
+      <Pressable
+        style={styles.buttonInnerContainer}
+        onPress={pressHandler}
+        android_ripple={{ color: "#640233" }}
+      >
+        <Text style={styles.textContainer}>{children}</Text>
+      </Pressable>
+    </View>
+  );
+};
+
+export default MainButton;
+
+styles = StyleSheet.create({
+  buttonOuterContainer: {
+    borderRadius: 28,
+    margin: 4,
+    overflow: "hidden",
+  },
+  buttonInnerContainer: {
+    backgroundColor: "#72063c",
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+    elevation: 2,
+  },
+  textContainer: {
+    color: "#fff",
+    textAlign: "center",
+  },
+});
---
+++ screens/StartGameScreen.js
@@ -0,0 +1,60 @@
+import { TextInput, View, StyleSheet } from "react-native";
+
+import MainButton from "../components/MainButton";
+
+const StartGameScreen = () => {
+  return (
+    <View style={styles.screen}>
+      <TextInput
+        style={styles.textInput}
+        maxLength={2}
+        keyboardType="number-pad"
+      />
+      <View style={styles.buttonsContainer}>
+        <View style={styles.buttonContainer}>
+          <MainButton>Reset</MainButton>
+        </View>
+        <View style={styles.buttonContainer}>
+          <MainButton>Confirm</MainButton>
+        </View>
+      </View>
+    </View>
+  );
+};
+
+export default StartGameScreen;
+
+styles = StyleSheet.create({
+  screen: {
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 100,
+    marginHorizontal: 24,
+    padding: 16,
+    backgroundColor: "#4e0329",
+    borderRadius: 8,
+    // works on android only
+    elevation: 2,
+    // works on ios only
+    // shadowColor: "black",
+    // shadowOffset: { width: 0, height: 2 },
+    // shadowRadius: 6,
+  },
+  textInput: {
+    height: 50,
+    width: 50,
+    fontSize: 32,
+    borderBottomColor: "#ddb52f",
+    borderBottomWidth: 2,
+    marginVertical: 8,
+    color: "#ddb52f",
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+  buttonsContainer: {
+    flexDirection: "row",
+  },
+  buttonContainer: {
+    flex: 1,
+  }
+});
